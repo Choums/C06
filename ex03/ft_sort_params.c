bb06aa2a2802c5e2201b32e85595100e857f3622
Chams <chams.aidel@outlook.fr>
2020-07-14 18:30:55 +0200

    finifini

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 14:59:02 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/14 17:46:18 by caidel           ###   ########.fr       */
+/*   Updated: 2020/07/14 18:30:07 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,21 @@ void	ft_putstr(char *str)
 	}
 }
 
-int	ft_strcmp(char *s1, char *s2)
+void	ft_print(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+
+}
+
+int		ft_strcmp(char *s1, char *s2)
 {
 	int i;
 
@@ -41,33 +55,24 @@ int	ft_strcmp(char *s1, char *s2)
 
 int		main(int argc, char *argv[])
 {
-	int j;
-	int i;
-	char *swap;
+	int		j;
+	int		i;
+	char	*swap;
 
 	i = 0;
-	while (i < argc - 1)
+	while (++i < argc)
 	{
-		j = 0;
-		while (j < (argc - i))
+		j = i;
+		while (++j < argc)
 		{
-			if (ft_strcmp(argv[i], argv[i + 1]) > 0) 
+			if (ft_strcmp(argv[i], argv[j]) > 0)
 			{
 				swap = argv[i];
-				argv[i] = argv[i + 1];
-				argv[i + 1] = swap;
+				argv[i] = argv[j];
+				argv[j] = swap;
 			}
-			j++;
 		}
-		i++;
 	}
-
-	i = 1;
-	while (i < argc)
-	{
-		ft_putstr(argv[i]);
-		ft_putchar('\n');
-		i++;
-	}
+	ft_print(argc, argv);
 	return (0);
 }
